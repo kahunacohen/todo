69f0846c5dc65411918bbde7be452a6c6a23e212
Aaron Cohen <acohen@mountainpasstech.com>
2019-07-15 13:30:47 +0300

    add todo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
 import React from 'react';
+import {ToDo} from './todo';
 
 function App() {
-  return (
-    <div className="container">
-      <div class="row">
-        <div class="col-md">
-          <h2>To Do List</h2>
-          <ul class="list-group">
-            <li class="list-group-item active">
-              <img src="./check.png"/><span class="todo-title done">Go to birthday party</span>
-            </li>
-            <li class="list-group-item">
-              <span class="todo-title to-do">Buy groceries</span>
-            </li>
-            <li class="list-group-item">
-              <span class="todo-title to-do">Buy casket</span></li>
-            <li class="list-group-item">
-              <span class="todo-title to-do">Pay electric bill</span></li>
-            <li class="list-group-item">
-              <span class="todo-title to-do">Set mouse trap</span></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  return (<ToDo/>);
 }
 
 export default App;
