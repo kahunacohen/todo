e3da944784ac67c9394e7ef0e1680b09aa9d2f4b
Aaron Cohen <acohen@mountainpasstech.com>
2019-07-15 10:57:28 +0300

    add check

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div className="container">
+      <div class="row">
+        <div class="col-md">
+          <h2>To Do List</h2>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <img src="./check.png"/><span class="todo-title done">Go to birthday party</span>
+            </li>
+            <li class="list-group-item">
+              <span class="todo-title to-do">Buy groceries</span>
+            </li>
+            <li class="list-group-item">
+              <span class="todo-title to-do">Buy casket</span></li>
+            <li class="list-group-item">
+              <span class="todo-title to-do">Pay electric bill</span></li>
+            <li class="list-group-item">
+              <span class="todo-title to-do">Set mouse trap</span></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
