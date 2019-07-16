6f80f2eaca3ebc4eaf6f86806317d0808d8d9d94
Aaron Cohen <acohen@mountainpasstech.com>
2019-07-16 16:12:33 +0300

    add actions

---
+++ src/actions.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Actions extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Actions</h2>
+        <ul id="actions">
+          <li>
+            <input ref={this.props.setAddInput} type="text" />
+            <button type="button" className="btn-sm btn-info" onClick={this.props.handleAddItem}>Add</button>
+          </li>
+          <li>
+            <button type="button" className="btn-sm btn-success" onClick={this.props.handleMarkDone}>Mark Done</button>
+          </li>
+          <li>
+            <button type="button" className="btn-sm btn-secondary" onClick={this.props.handleMarkUndone}>Mark Undone</button>
+          </li>
+          <li>
+            <button type="button" className="btn-sm btn-danger" onClick={this.props.handleDeleteItem}>Delete</button>
+          </li>
+        </ul></div>);
+  }
+}
